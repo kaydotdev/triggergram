0849e88aa02ec6d1f4baff46a82327adc190d578
Anton Kozyriev <sonara4mus@gmail.com>
2021-08-22 17:46:40 +0300

    feat(TriggergramContext): media post map configurations

--- triggers/core/Data/Context/TriggergramContext.cs
+++ triggers/core/Data/Context/TriggergramContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MediaPostEntity = Triggergram.Core.Data.Models.MediaPost;
+using Triggergram.Core.Data.Configs;
 
 namespace Triggergram.Core.Data.Context
 {
@@ -9,5 +10,12 @@ namespace Triggergram.Core.Data.Context
 
         public TriggergramContext(DbContextOptions<TriggergramContext> options)
             : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new MediaPostMap());
+        }
     }
 }
