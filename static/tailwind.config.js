6473248e1f616dba4ef7fce7b55cedb4cf843abb
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-09 19:17:49 +0300

    feat(tailwind.config.js): set custom triade color pallete

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          "primary": "#570DF8",
-          "secondary": "#F000B8",
+          "primary": "#FE8F01",
+          "secondary": "#4E91E6",
           "accent": "#37CDBE",
           "neutral": "#3D4451",
           "base-100": "#FFFFFF",
