4a6e1175cda37c2bbeed693d8002f78753205fb2
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-09 19:42:34 +0300

    feat: application structure definition

--- static/src/App.tsx
+++ static/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
 function App() {
   return (
-    <div>
-      Triggergram
+    <div className="w-screen h-screen flex flex-col bg-base-100 p-2">
+      <div className="basis-16">Navbar</div>
+      <div className="flex-1 overflow-y-auto overflow-x-hidden bg-slate-100"></div>
+      <div className="basis-16">Footer</div>
     </div>
   );
 }
