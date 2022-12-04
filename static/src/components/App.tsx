b49db9314ac4297338b83627cf23e9d8a6f78b91
Anton Kozyriev <sonara4mus@gmail.com>
2022-12-05 01:41:57 +0200

    env(static): migrate to the vite bundler

---
+++ static/src/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-row w-full h-full justify-center items-center">
+      <div><img src="/icon.svg" alt="banner" /></div>
+    </div>
+  )
+}
+
+export default App
